79605fe7cef8e6d7b8f0170bb590ad0d946909bd
Danilo Henrique <danilohenriqueoliveirasantos@gmail.com>
2019-04-16 11:27:52 -0300

    Adição de composição entre interface e classes

--- bytebank-herdado/src/Admistrador.java
+++ bytebank-herdado/src/Admistrador.java
@@ -1,7 +1,11 @@
 
 public class Admistrador extends Funcionario implements Autenticavel {
 
-	private int senha;
+	private AutenticacaoUtil autenticador;
+	
+	public Admistrador() {
+		this.autenticador = new AutenticacaoUtil();
+	}
 
 	@Override
 	public double getBonificacao() {
@@ -10,15 +14,13 @@ public class Admistrador extends Funcionario implements Autenticavel {
 
 	@Override
 	public void setSenha(int senha) {
-		this.senha = senha;
+		this.autenticador.setSenha(senha);
+
 	}
 
 	@Override
 	public boolean autentica(int senha) {
-		if (this.senha == senha) {
-			return true;
-		}
-		return false;
+		return this.autenticador.autentica(senha);
 	}
 
 }
