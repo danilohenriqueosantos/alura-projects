867cebd84d93e7531980345adf619905265014ee
danilohenriqueosantos <43797763+danilohenriqueosantos@users.noreply.github.com>
2019-04-22 15:24:55 -0300

    Update TesteString.java
    Metodos da classe String

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteString.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteString.java
@@ -6,11 +6,26 @@ public class TesteString {
 		
 		String nome = "Danilo";
 		
-		String nomeConvertido = nome.replace("D", "d");
+//		char c = 'D';
+//		char d = 'A';
+//		
+//		String nomeConvertido = nome.replace(c, d);
 		
+		System.out.println(nome.indexOf("a"));
+		
+		System.out.println(nome.substring(3));
+		
+		System.out.println(nome.isEmpty());
+		
+		System.out.println(nome.length());
+		System.out.println(nome.contains("a"));
+		
+		for (int i = 0; i < nome.length(); i ++) {
+			System.out.println(nome.charAt(i));
+		}
 //		nomeConvertido.toLowerCase();
 		
-		System.out.println(nomeConvertido);
+//		System.out.println(nomeConvertido);
 	}
 
 }
