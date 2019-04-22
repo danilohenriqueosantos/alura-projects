f9f4948053a6f400ccfbe01b6aa9cb1f84bdf660
danilohenriqueosantos <43797763+danilohenriqueosantos@users.noreply.github.com>
2019-04-22 15:07:12 -0300

    Strings
    Trabalhando com a classe String

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteString.java
@@ -0,0 +1,16 @@
+package br.com.bytebank.banco.test;
+
+public class TesteString {
+	
+	public static void main(String[] args) {
+		
+		String nome = "Danilo";
+		
+		String nomeConvertido = nome.replace("D", "d");
+		
+//		nomeConvertido.toLowerCase();
+		
+		System.out.println(nomeConvertido);
+	}
+
+}
