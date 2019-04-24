7ee43f732d326462c25604e3833ab62e76e5c3f8
Danilo Santos <danilohenriqueoliveirasantos@gmail.com>
2019-04-23 22:55:56 -0300

    Implementando o método de comparação de objetos

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
@@ -1,30 +1,68 @@
 package br.com.bytebank.banco.test.util;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class Teste {
 
 	public static void main(String[] args) {
+
+		Conta contaCorrente = new ContaCorrente(111, 444);
+		contaCorrente.deposita(100.0);
+
+		Conta contaPoupanca = new ContaPoupanca(111, 333);
+		contaPoupanca.deposita(150.0);
+
+		Conta contaCorrente2 = new ContaCorrente(111, 222);
+		contaCorrente2.deposita(130.0);
+
+		Conta contaPoupanca2 = new ContaPoupanca(111, 555);
+		contaPoupanca2.deposita(160.0);
+
+		List<Conta> lista = new ArrayList<Conta>();
+
+		lista.add(contaCorrente);
+		lista.add(contaPoupanca);
+		lista.add(contaCorrente2);
+		lista.add(contaPoupanca2);
 		
-		int[] idades = new int[5];
+		for (Conta conta : lista) {
+			System.out.println(conta);
+			
+		}
 		
-		String[] nomes = new String[5];
+		NumeroDaContaComparator numeroDaContaComparator = new NumeroDaContaComparator();
 		
-		int idade = 29;
+		lista.sort(numeroDaContaComparator);
 		
-		Integer idadeRef = Integer.valueOf(29);
-		int intValue = idadeRef.intValue();
+		System.out.println("----------------");
 		
-		String s = args[0];
-		
-//		Integer numero = Integer.valueOf(s);
-		
-		int numero = Integer.parseInt(s);
-		
-		System.out.println(numero);
-		
-		List<Integer> numeros = new ArrayList<Integer>();
-		numeros.add(idade);
+		for (Conta conta : lista) {
+			System.out.println(conta);
+			
+		}
+
 	}
+}
+
+class NumeroDaContaComparator implements Comparator<Conta> {
+
+	@Override
+	public int compare(Conta conta1, Conta conta2) {
+
+		if (conta1.getNumero() < conta2.getNumero()) {
+			return -1;
+		}
+		if (conta1.getNumero() > conta2.getNumero()) {
+			return 1;
+		}
+
+		return 0;
+	}
+
 }
