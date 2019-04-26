d490fa063ec5dbce486b13b38a0880a0bbb295c0
danilohenriqueosantos <43797763+danilohenriqueosantos@users.noreply.github.com>
2019-04-26 17:50:55 -0300

    Equal e hashcode

--- gerenciador-de-curso/src/br/com/alura/TestaCursoComAluno.java
+++ gerenciador-de-curso/src/br/com/alura/TestaCursoComAluno.java
@@ -10,19 +10,30 @@ public class TestaCursoComAluno {
 		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
 		javaColecoes.adiciona(new Aula("Criando uma aula", 20));
 		javaColecoes.adiciona(new Aula("Modelando com coleções", 24));
-		
+
 		Aluno aluno = new Aluno("Rodrigo Turini", 34672);
 		Aluno aluno2 = new Aluno("Guilherme Silveira", 5617);
 		Aluno aluno3 = new Aluno("Mauricio Aniche", 17645);
-		
+
 		javaColecoes.matricula(aluno);
 		javaColecoes.matricula(aluno2);
 		javaColecoes.matricula(aluno3);
-		
+
 		System.out.println("Todos os alunos matriculados: ");
 		javaColecoes.getAlunos().forEach(alunos -> {
 			System.out.println(alunos);
 		});
+
+		System.out.println("O aluno " + aluno + " está matriculado?");
+		System.out.println(javaColecoes.estaMatriculado(aluno));
+
+		Aluno turini = new Aluno("Rodrigo Turini", 34672);
+		System.out.println("E esse Turini, está matriculado?");
+		System.out.println(javaColecoes.estaMatriculado(turini));
+
+		System.out.println("O aluno é equals ao Turini?");
+		System.out.println(aluno.equals(turini));
+		System.out.println(aluno.hashCode() == turini.hashCode());
 
 	}
 }
