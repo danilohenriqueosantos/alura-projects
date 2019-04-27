caf79d6da03c480b2befaffedd19345c07a32b94
Danilo Santos <danilohenriqueoliveirasantos@gmail.com>
2019-04-27 12:20:56 -0300

    Exercicios Java da internet

---
+++ exercicios/src/br/com/exercicios/modelos/InverteNumeros.java
@@ -0,0 +1,13 @@
+package br.com.exercicios.modelos;
+
+public class InverteNumeros {
+
+	public InverteNumeros() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void inverte(int x, int y) {
+		System.out.print(y);
+		System.out.print(x);
+	}
+}
---
+++ exercicios/src/br/com/exercicios/testes/TestaMaiorNumero.java
@@ -0,0 +1,14 @@
+package br.com.exercicios.testes;
+
+import br.com.exercicios.modelos.MaiorNumero;
+
+public class TestaMaiorNumero {
+
+	public static void main(String[] args) {
+		
+		MaiorNumero maiorNumero = new MaiorNumero();
+		maiorNumero.maiorNumero(3, 2);
+		maiorNumero.maiorNumero(1, 2);
+		maiorNumero.maiorNumero(5, 5);
+	}
+}
