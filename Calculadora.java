04e72d3713a34e4522e7260336a01c4a61bb51e1
danilohenriqueosantos <43797763+danilohenriqueosantos@users.noreply.github.com>
2019-04-29 12:12:36 -0300

    Testes de Maven

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,6 @@
 public class Calculadora {
 
     public static void main(String[] args) {
-        System.out.println("Cinco mais cinco é igual a " (5 + 5));
+        System.out.println("Cinco mais cinco é igual a " + (5 + 5));
     }
 }
