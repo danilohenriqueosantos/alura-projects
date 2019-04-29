78064befb28b2ba81675d63812fc7d425e1472a5
danilohenriqueosantos <43797763+danilohenriqueosantos@users.noreply.github.com>
2019-04-29 14:37:35 -0300

    Utilizando o comando mvn pmd:pmd

---
+++ produtos/src/main/java/br/alura/maven/produtos/Produto.java
@@ -0,0 +1,14 @@
+package br.alura.maven.produtos;
+
+public class Produto {
+
+	private final String nome;
+	private final double preco;
+
+	public Produto(String nome, double preco) {
+
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+}
