206149768d5c07ce385fba5c09d973a4651d91a3
danilohenriqueosantos <43797763+danilohenriqueosantos@users.noreply.github.com>
2019-04-30 11:08:38 -0300

    Criando uma resposta no prjeto Maven via Servlet

--- lojaweb/src/main/java/br/com/alura/maven/lojaweb/ContatoServlet.java
+++ lojaweb/src/main/java/br/com/alura/maven/lojaweb/ContatoServlet.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import br.alura.maven.Produto;
-
 @WebServlet(urlPatterns = {"/contato"})
 public class ContatoServlet extends HttpServlet {
 
@@ -21,8 +19,6 @@ public class ContatoServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		Produto produto = new Produto("bala", 15.0);
 		
 		PrintWriter writer = resp.getWriter();
 		writer.println("<html><h2>Bata um papo conosco</h2></html>");
