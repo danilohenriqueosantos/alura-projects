cf39f7e2dbadef4f5735309600eedf7ab7114581
danilohenriqueosantos <43797763+danilohenriqueosantos@users.noreply.github.com>
2019-04-30 16:47:30 -0300

    Criaçao da classe banco, Empresa e Servlet listaEmpresa

--- gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -16,12 +16,18 @@ import javax.servlet.http.HttpServletResponse;
 public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response)
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		System.out.println("Cadastrando nova empresa");
 		
 		String nomeEmpresa = request.getParameter("nome");
+		
+		Empresa empresa = new Empresa();
+		empresa.setNome(nomeEmpresa);
+		
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
 
 		PrintWriter writer = response.getWriter();
 		writer.println("<html><body>Empresa " + nomeEmpresa + " cadastrada  com sucesso! </body></html>");
