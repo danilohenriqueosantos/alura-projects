3de71b3aab4c7f65c685f75f38e64a4f2134b1f5
danilohenriqueosantos <43797763+danilohenriqueosantos@users.noreply.github.com>
2019-04-30 15:05:43 -0300

    Criando servlet de resposta oi

---
+++ gerenciador/src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,28 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class OiMundoServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html>");
+		writer.println("<body>");
+		writer.println("Oi mundo, parabéns você escreveu o primeiro Servlet.");
+		writer.println("</body>");
+		writer.println("</html>");
+		writer.close();
+		
+	
+	}
+
+}
