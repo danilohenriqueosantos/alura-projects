aeba6dcdc62c201dfbc92ab4432b1b715e54b145
Danilo Santos <danilohenriqueoliveirasantos@gmail.com>
2019-05-05 12:57:22 -0300

    Autorizando o acesso

--- gerenciador/src/br/com/alura/gerenciador/acao/ListaEmpresas.java
+++ gerenciador/src/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -16,10 +16,7 @@ public class ListaEmpresas implements Acao {
 	
 	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		HttpSession sessao = request.getSession();
-		if(sessao.getAttribute("usuarioLogado") == null) {
-			return "redirect:entrada?acao=LoginForm";
-		}
+		
 		
 		System.out.println("listando empresas");
 		
