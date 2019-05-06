453d5e891113dc9cab1dcd1a5f8ae0e153b84ca0
danilohenriqueosantos <43797763+danilohenriqueosantos@users.noreply.github.com>
2019-05-06 15:09:22 -0300

    Cliente do serviço

--- gerenciador/src/br/com/alura/gerenciador/servlet/EmpresasService.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/EmpresasService.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
@@ -22,24 +23,36 @@ import br.com.alura.gerenciador.modelo.Empresa;
 public class EmpresasService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		List<Empresa> empresas = new Banco().getEmpresas();
+
+		String valor = request.getHeader("Accept");
 		
-		XStream xStream = new XStream(new DomDriver());
-		xStream.alias("empresa", Empresa.class);
-		String xml = xStream.toXML(empresas);
-		
-		response.setContentType("application/xml");
-		response.getWriter().print(xml);
-		
-//		Gson gson = new Gson();
-//		String json = gson.toJson(empresas);
-//		
-//		response.setContentType("application/json");
-//		response.getWriter().print(json);
-		
+		System.out.println(valor);
+
+		if (valor.contains("xml")) {
+			XStream xStream = new XStream(new DomDriver());
+			xStream.alias("empresa", Empresa.class);
+			String xml = xStream.toXML(empresas);
+
+			response.setContentType("application/xml");
+			response.getWriter().print(xml);
+			
+		} else if (valor.endsWith("json")) {
+
+			Gson gson = new Gson();
+			String json = gson.toJson(empresas);
+
+			response.setContentType("application/json");
+			response.getWriter().print(json);
+			
+		} else {
+			response.setContentType("application/json");
+			response.getWriter().print("{'message':'no content'}");
+		}
+
 	}
 
 }
