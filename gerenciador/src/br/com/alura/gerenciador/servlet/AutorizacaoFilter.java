90489bcad9cc41d1daf6bf0788e1ff0e442e3834
Danilo Santos <danilohenriqueoliveirasantos@gmail.com>
2019-05-06 21:05:38 -0300

    Projeto gerenciador

--- gerenciador/src/br/com/alura/gerenciador/servlet/AutorizacaoFilter.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/AutorizacaoFilter.java
@@ -4,21 +4,24 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- * Servlet Filter implementation class AutorizacaoFilter
- */
-@WebFilter("/entrada")
+
+//@WebFilter("/entrada")
 public class AutorizacaoFilter implements Filter {
 
- 
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {}
+	
+	@Override
+	public void destroy() {}
+	
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 
 		System.out.println("AutorizaçãoFilter");
