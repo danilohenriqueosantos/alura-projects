4ee9538ffbf6d6f3c48fdb9779011698d95e2f3e
Danilo Santos <danilohenriqueoliveirasantos@gmail.com>
2019-05-07 22:37:38 -0300

    MovimentacaoDao

--- financas/src/br/com/caelum/financas/teste/TesteFuncoesJPQL.java
+++ financas/src/br/com/caelum/financas/teste/TesteFuncoesJPQL.java
@@ -3,9 +3,8 @@ package br.com.caelum.financas.teste;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
 
+import br.com.caelum.financas.dao.MovimentacaoDao;
 import br.com.caelum.financas.modelo.Conta;
 import br.com.caelum.financas.modelo.TipoMovimentacao;
 import br.com.caelum.financas.util.JPAUtil;
@@ -20,16 +19,9 @@ public class TesteFuncoesJPQL {
 		Conta conta = new Conta();
 		conta.setId(2);
 		
-		String jpql = "select avg(m.valor) from Movimentacao m where m.conta = :pConta" + 
-		" and m.tipo = :pTipo" +
-		" group by day(m.data), month(m.data), year(m.data)";
 		
-	    TypedQuery<Double> query = em.createQuery(jpql, Double.class);
-	    query.setParameter("pConta", conta);
-	    query.setParameter("pTipo", TipoMovimentacao.SAIDA);
-	    
-	   List<Double> medias = (List<Double>) query.getResultList();
-	    
+		MovimentacaoDao dao = new MovimentacaoDao();
+		List<Double> medias = dao .getMediaPorDiasETipo(TipoMovimentacao.SAIDA, conta);	    
 	    System.out.println("A média do dia 26 é: " + medias.get(0));
 	    System.out.println("A média do dia 27 é: " + medias.get(1));
 	    
