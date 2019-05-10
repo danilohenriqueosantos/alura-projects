4fe1f0f3d5ca15758c6911b1cb58546a5a7d877e
danilohenriqueosantos <43797763+danilohenriqueosantos@users.noreply.github.com>
2019-05-10 11:19:18 -0300

    Implantando e complementando o controller

--- casadocodigo/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
+++ casadocodigo/casadocodigo/src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+	
 	@RequestMapping("/")
-	public void index() {
+	public String index() {
 		System.out.println("Entrando na Home da Casa do Código");
+		return "home";
 	}
 }
