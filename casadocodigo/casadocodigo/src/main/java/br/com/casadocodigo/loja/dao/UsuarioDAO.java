976d1e8bc84451158af5d6d8078f417d4a4fd3db
danilohenriqueosantos <43797763+danilohenriqueosantos@users.noreply.github.com>
2019-05-20 10:59:47 -0300

    Finalizando a aula de usuário e UsuárioDao, funcionando

--- casadocodigo/casadocodigo/src/main/java/br/com/casadocodigo/loja/dao/UsuarioDAO.java
+++ casadocodigo/casadocodigo/src/main/java/br/com/casadocodigo/loja/dao/UsuarioDAO.java
@@ -19,7 +19,7 @@ public class UsuarioDAO implements UserDetailsService{
 	
 	public Usuario loadUserByUsername(String email) {
 		
-		List<Usuario> usuarios = manager.createQuery("select u from Usuario where u.email = :email", Usuario.class)
+		List<Usuario> usuarios = manager.createQuery("select u from Usuario u where u.email = :email", Usuario.class)
 		.setParameter("email", email)
 		.getResultList();
 		
